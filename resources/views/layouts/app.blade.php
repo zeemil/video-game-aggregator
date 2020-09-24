01b719f59abd6e8566d770f0a7ffd029861bea49
Emilien Fritschy <Emilien.Fritschy@bewan.be>
2020-09-24 11:11:42 +0200

    Setup app layout

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Laracasts Video Games</title>
+
+    <link rel="stylesheet" href="/css/app.css">
+
+</head>
+<body class="bg-gray-900 text-white">
+
+    <header class="border-b border-gray-800">
+        <nav class="container mx-auto flex items-center justify-between px-4 py-6">
+            <div class="flex items-center">
+                <a href="/">
+                    <img src="/laracasts-logo.svg" alt="laracasts" class="w-32 flex-none">
+                </a>
+
+                <ul class="flex ml-16 space-x-8">
+                    <li><a href="#" class="hover:text-gray-400">Games</a></li>
+                    <li><a href="#" class="hover:text-gray-400">Reviews</a></li>
+                    <li><a href="#" class="hover:text-gray-400">Comming Soon</a></li>
+                </ul>
+            </div>
+        </nav>
+    </header>
+
+</body>
+</html>
