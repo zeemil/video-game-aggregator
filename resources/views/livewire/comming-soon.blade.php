b20ca8829a87eb863d09291884671099ee84f7af
Emilien Fritschy <Emilien.Fritschy@bewan.be>
2020-10-01 18:52:40 +0200

    Skeleton loader for other elements

--- resources/views/livewire/comming-soon.blade.php
+++ resources/views/livewire/comming-soon.blade.php
@@ -15,6 +15,16 @@
             </div>
         </div>
     @empty
-        <div class="spinner mt-8"></div>
+        @foreach(range(1,3) as $game)
+            <div class="game flex">
+                <div class="bg-gray-800 w-16 h-24"></div>
+                <div class="ml-4">
+                    <div class="text-transparent text-lg bg-gray-700 rounded">Game name</div>
+                    <div class="inline-block text-transparent text-lg bg-gray-700 rounded text-sm mt-1">
+                        Date
+                    </div>
+                </div>
+            </div>
+        @endforeach
     @endforelse
 </div>
--- resources/views/livewire/recently-reviewed.blade.php
+++ resources/views/livewire/recently-reviewed.blade.php
@@ -1,12 +1,14 @@
-<div  wire:init="loadRecentlyReviewed" class="recently-reviewed-container space-y-12 mt-8">
+<div wire:init="loadRecentlyReviewed" class="recently-reviewed-container space-y-12 mt-8">
     @forelse($recentlyReviewed as $game)
         <div class="game bg-gray-800 rounded-lg shadow-md flex px-6 py-6">
             <div class="relative flex-none">
                 <a href="#">
-                    <img src="{{ Str::replaceFirst('thumb', 'cover_big',$game['cover']['url']) }}" alt="game cover" class="w-48 hover:opacity-75 transition ease-in-out duration-150">
+                    <img src="{{ Str::replaceFirst('thumb', 'cover_big',$game['cover']['url']) }}" alt="game cover"
+                         class="w-48 hover:opacity-75 transition ease-in-out duration-150">
                 </a>
                 @if(isset($game['rating']))
-                    <div class="absolute bottom-0 right-0 w-16 h-16 bg-gray-900 rounded-full" style="right: -20px; bottom: -20px">
+                    <div class="absolute bottom-0 right-0 w-16 h-16 bg-gray-900 rounded-full"
+                         style="right: -20px; bottom: -20px">
                         <div class="font-semibold text-xs flex justify-center items-center h-full">
                             {{ round($game['rating']).'%' }}
                         </div>
@@ -15,7 +17,8 @@
             </div>
 
             <div class="ml-12">
-                <a href="#" class="block text-lg font-semibold leading-tight hover:text-gray-400 mt-8">{{$game['name']}}</a>
+                <a href="#"
+                   class="block text-lg font-semibold leading-tight hover:text-gray-400 mt-8">{{$game['name']}}</a>
                 <div class="text-gray-400 mt-1">
                     @foreach($game['platforms'] as $plateform)
                         @if(array_key_exists('abbreviation', $plateform))
@@ -29,8 +32,33 @@
             </div>
 
         </div>
-    @empty
-        <div class="spinner mt-8"></div>
-    @endforelse
+        @empty
+            @foreach(range(1,3) as $game)
+                <div class="game bg-gray-800 rounded-lg shadow-md flex px-6 py-6">
+                    <div class="relative flex-none">
+                        <div class="bg-gray-700 w-32 lg:w-48 h-40 lg:h-56"></div>
+                    </div>
+
+                    <div class="ml-12">
+                        <div class="inline-block text-lg font-semibold leading-tight text-transparent bg-gray-700 rounded mt-8">
+                            Game name
+                        </div>
+
+                        <div class="mt-8 space-y-4 hidden lg:block">
+                        <span class="text-transparent bg-gray-700 rounded inline-block">
+                             In to am attended desirous raptures declared diverted confined at.
+                        </span>
+                            <span class="text-transparent bg-gray-700 rounded inline-block">
+                             In to am attended desirous raptures declared diverted confined at.
+                        </span>
+                            <span class="text-transparent bg-gray-700 rounded inline-block">
+                             In to am attended desirous raptures declared diverted confined at.
+                        </span>
+                        </div>
+                    </div>
+
+                </div>
+            @endforeach
+        @endforelse
 
 </div>
